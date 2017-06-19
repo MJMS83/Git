3e759fd5a24ee84e8658ad347c58e8146a84c3fa
William Méndez <will.mendez@hotmail.com>
2017-06-19 17:24:02 -0600

    Ingresar datos de tomas

--- app/src/main/java/una/ac/cr/git/MainActivity.java
+++ app/src/main/java/una/ac/cr/git/MainActivity.java
@@ -5,15 +5,25 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.Normalizer;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     //La pantalla principal es la de tomas de presion
+    TextView sistolica;
+    TextView diastolica;
+    TextView fecha;
+    ListView lista;
 
     @Override
 
@@ -25,25 +35,160 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        sistolica = (TextView) findViewById(R.id.txtSistolica);
+        diastolica = (TextView) findViewById(R.id.txtDiastolica);
+        fecha = (TextView) findViewById(R.id.txtFecha);
+        lista = (ListView) findViewById(R.id.ListViewTomas);
+
+
+        final ArrayList<TomaPresion> datos = new ArrayList<>();
+        lista.setAdapter(new Lista_Adaptador(this, R.layout.entrada, datos) {
+            @Override
+            public void onEntrada(Object activity_main, View view) {
+                TextView txtsistolica = (TextView) view.findViewById(R.id.ViewSistolica);
+                txtsistolica.setText(((TomaPresion)activity_main).getSistolica());
+
+                TextView txtDiastolica = (TextView) view.findViewById(R.id.ViewDiastolica);
+                txtDiastolica.setText(((TomaPresion)activity_main).getDiastolica());
+
+                TextView txtCondicion = (TextView) view.findViewById(R.id.ViewCondicion);
+                txtCondicion.setText(((TomaPresion)activity_main).getCondicion());
+
+
+                switch (((TomaPresion)activity_main).getCondicion()){
+                    case "Optima":
+                        txtsistolica.setClickable(true);
+                        txtsistolica.setBackgroundColor(getResources().getColor(R.color.Normal));
+                        txtDiastolica.setBackgroundColor(getResources().getColor(R.color.Normal));
+                        txtCondicion.setBackgroundColor(getResources().getColor(R.color.Normal));
+
+
+                        break;
+                    case "Normal":
+                        txtsistolica.setClickable(true);
+                        txtsistolica.setBackgroundColor(getResources().getColor(R.color.Normal));
+                        txtDiastolica.setBackgroundColor(getResources().getColor(R.color.Normal));
+                        txtCondicion.setBackgroundColor(getResources().getColor(R.color.Normal));
+
+                        break;
+                    case "Normal-Alta":
+                        txtsistolica.setBackgroundColor(getResources().getColor(R.color.NormalAlta));
+                        txtDiastolica.setBackgroundColor(getResources().getColor(R.color.NormalAlta));
+                        txtCondicion.setBackgroundColor(getResources().getColor(R.color.NormalAlta));
+
+                        break;
+                    case "Fase 1":
+                        txtsistolica.setBackgroundColor(getResources().getColor(R.color.fase1));
+                        txtDiastolica.setBackgroundColor(getResources().getColor(R.color.fase1));
+                        txtCondicion.setBackgroundColor(getResources().getColor(R.color.fase1));
+
+                        break;
+                    case "Fase 2":
+                        txtsistolica.setBackgroundColor(getResources().getColor(R.color.Fase2));
+                        txtDiastolica.setBackgroundColor(getResources().getColor(R.color.Fase2));
+                        txtCondicion.setBackgroundColor(getResources().getColor(R.color.Fase2));
+
+                        break;
+                    case "Fase 3":
+                        txtsistolica.setBackgroundColor(getResources().getColor(R.color.Fase3));
+                        txtDiastolica.setBackgroundColor(getResources().getColor(R.color.Fase3));
+                        txtCondicion.setBackgroundColor(getResources().getColor(R.color.Fase3));
+
+                        break;
+                    case "Sistolica Aislada":
+                        txtsistolica.setBackgroundColor(getResources().getColor(R.color.SistoAislada));
+                        txtsistolica.setTextColor(getResources().getColor(R.color.Titulo));
+                        txtDiastolica.setBackgroundColor(getResources().getColor(R.color.SistoAislada));
+                        txtDiastolica.setTextColor(getResources().getColor(R.color.Titulo));
+                        txtCondicion.setBackgroundColor(getResources().getColor(R.color.SistoAislada));
+                        txtCondicion.setTextColor(getResources().getColor(R.color.Titulo));
+                        break;
+                }
+
+            }
+        });
 
 
 
 
+        final Button calcular = (Button) findViewById(R.id.calcular);
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Condicion()=="Vacio"){
+                    Toast toast1 = Toast.makeText(getApplicationContext(),"Ha dejado campos vacíos", Toast.LENGTH_SHORT);
+                    toast1.setGravity(Gravity.CENTER|Gravity.CENTER,0,0);
+                    toast1.show();
+                }else if (Condicion()=="Error"){
+                    Toast toast1 = Toast.makeText(getApplicationContext(),"Los datos son incorrectos", Toast.LENGTH_SHORT);
+                    toast1.setGravity(Gravity.CENTER|Gravity.CENTER,0,0);
+                    toast1.show();
+                }
+                else {
+
+                    datos.add(new TomaPresion(sistolica.getText().toString(), diastolica.getText().toString(), Condicion(), fecha.getText().toString()));
+                    //Ocultar el boton para que pueda ingresar solo una vez los datos
+                    calcular.setVisibility(View.INVISIBLE);
+                    //OCULTA EL TECLADO AL DAR CLIC
+                    InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
 
 
 
 
+                }
 
 
 
+
+            }
+        });
 
 
     }
 
 
 
+    public String Condicion() {
 
+         /*Convertir los valores del testview a enteros*/
+        int sisto = sistolica.getText().toString() != null && !sistolica.getText().toString().equals("") ? Integer.parseInt(sistolica.getText().toString()) : -1;
+        int diasto = sistolica.getText().toString() != null && !diastolica.getText().toString().equals("") ? Integer.parseInt(diastolica.getText().toString()) : -1;
 
+        for (int i = 0; i < 3; i++) {
+            if ((sisto<0) && (diasto<0)) {
+                return "Vacio";
+            }else
+            if ((sisto < 120) && (diasto < 80)) {
+                // BackgroundColorSpan b = new BackgroundColorSpan(color);
+                return "Optima";
+            } else /*NORMAL*/ {
+                if ((sisto < 130 && sisto >= 120) && (diasto >= 80 && diasto < 85)) {
+                    return "Normal";
+
+                } else /*NORMAL-ALTA*/
+                    if ((sisto >= 130 && sisto < 140) && (diasto >= 85 && diasto < 90)) {
+                        return "Normal-Alta";
+                    } else /*GRADO1*/
+                        if ((sisto >= 140 && sisto < 160) && (diasto >= 90 && diasto < 100)) {
+                            return "Fase 1";
+                        } else /*GRADO2*/
+                            if ((sisto >= 160 && sisto < 180) && (diasto >= 100 && diasto < 109)) {
+                                return "Fase 2";
+                            } else
+                            /*GRADO3*/
+                                if ((sisto >= 180) && (diasto >= 110)) {
+                                    return "Fase 3";
+                                } else /*SISTOLICA AISLADA*/
+                                    if ((sisto >= 140) && (diasto < 90)) {
+                                        return "Sistolica Aislada";
+                                    } else { /*MUESTRA UN ERROR SI NO SE DAN LAS CONDICIONES*/
+                                        return "Error";
+                                    }
+            }
+        }
+        return "";
+    }
 
 
     /*MENU PRINCIPAL*/
---
+++ app/src/main/java/una/ac/cr/git/TomaPresion.java
@@ -0,0 +1,60 @@
+package una.ac.cr.git;
+
+/**
+ * Created by WILLIAM on 19/06/2017.
+ */
+
+public class TomaPresion {
+
+    private String sistolica;
+    private String diastolica;
+    private String fechaToma;
+    private String condicion;
+
+    public TomaPresion() {
+    }
+
+    public TomaPresion(String sistolica, String diastolica, String condicion, String fechaToma) {
+        this.sistolica = sistolica;
+        this.diastolica = diastolica;
+        this.condicion = condicion;
+        this.fechaToma = fechaToma;
+    }
+
+    public TomaPresion(String sistolica, String diastolica, String condicion) {
+        this.sistolica = sistolica;
+        this.diastolica = diastolica;
+        this.condicion = condicion;
+    }
+
+    public String getCondicion() {
+        return condicion;
+    }
+
+    public void setCondicion(String condicion) {this.condicion = condicion;
+    }
+
+    public String getSistolica() {
+        return sistolica;
+    }
+
+    public void setSistolica(String sistolica) {
+        this.sistolica = sistolica;
+    }
+
+    public String getDiastolica() {
+        return diastolica;
+    }
+
+    public void setDiastolica(String diastolica) {
+        this.diastolica = diastolica;
+    }
+
+    public String getFechaToma() {
+        return fechaToma;
+    }
+
+    public void setFechaToma(String fechaToma) {
+        this.fechaToma = fechaToma;
+    }
+}
